13f2d3f3d11f79b58c8baa4eddc148ffca647cf3
Joel Eisner <joel@Joels-MBP.joeleisner.com>
2016-03-29 17:57:44 -0400

    Updated index.js to include --mature, --title, and --partnered option flags

--- index.js
+++ index.js
@@ -8,11 +8,14 @@ var chalk = require('chalk'),
 
 // Program (Commander)
 program
-    .version('1.0.0')
+    .version('1.1.0')
     .arguments('<twitch-username>')
     .option('-g, --game', 'Shows the game the streamer is playing')
     .option('-v, --viewers', 'Shows the amount of viewers the streamer currently has watching')
     .option('-s, --started', 'Shows when the streamer went live')
+    .option('-m, --mature', 'Shows whether the streamer streams mature content or not')
+    .option('-t, --title', 'Shows the current stream title')
+    .option('-p, --partnered', 'Shows whether the streamer is partnered or not')
     .action(function (streamer) {
         request
             .post('https://api.twitch.tv/kraken/streams/' + streamer)
@@ -27,6 +30,25 @@ program
                     if (program.game) {output += '\n' + chalk.blue('Game: ') + stream["game"];}
                     if (program.viewers) {output += '\n' + chalk.blue('Viewers: ') + stream["viewers"];}
                     if (program.started) {output += '\n' + chalk.blue('Started: ') + firstUp(timeago(new Date(stream["created_at"])))}
+                    if (program.mature) {
+                        var msg;
+                        if (stream["channel"]["mature"]) {
+                            msg = 'This streamer streams mature content';
+                        } else {
+                            msg = 'This streamer does not stream mature content';
+                        }
+                        output += '\n' + chalk.blue('Mature?: ') + msg;
+                    }
+                    if (program.title) {output += '\n' + chalk.blue('Title: ') + stream["channel"]["status"];}
+                    if (program.partnered) {
+                        var msg;
+                        if (stream["channel"]["partner"]) {
+                            msg = 'This streamer is partnered';
+                        } else {
+                            msg = 'This streamer is not partnered';
+                        }
+                        output += '\n' + chalk.blue('Partnered?: ') + msg;
+                    }
                 }
                 console.log(output);
             });
